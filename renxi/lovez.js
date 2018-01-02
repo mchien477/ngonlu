06be8b2077d010d4d4e78407d8ad4593c6be7613
mchien477 <minhchien572@gmail.com>
2018-01-02 20:24:53 +0700

    Update and rename love.js to lovez.js

--- renxi/lovez.js
+++ renxi/lovez.js
@@ -168,8 +168,8 @@
 
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
-            ctx.font = "12px 微软雅黑,Verdana";
-            ctx.fillText("Come Baby", 23, 10);
+            ctx.font = "12px Verdana";
+            ctx.fillText("Chạm vào :)", 23, 10);
             ctx.restore();
         },
         clear: function() {
